98a0a8bf97c6e3d98a66782432c8b589957a43b7
h701814234 <h701814234@localhost>
2013-09-09 13:20:13 -0400

    Initial commit

---
+++ CSC290_H1/helloworld.c
@@ -0,0 +1,17 @@
+/*
+ * helloworld.c
+ *
+ *  Created on: Sep 9, 2013
+ *      Author: h701814234
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int arc, char *argv[])
+{
+	printf("hello,world/n");
+	return (0);
+
+}
+
